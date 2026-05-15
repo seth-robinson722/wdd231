e89741277a5cd7d7967e435894545c0726e6de8b
seth-robinson722 <srobinson4@byupathway.edu>
2026-05-15 12:32:47 -0500

    course verification

--- scripts/course.js
+++ scripts/course.js
@@ -2,7 +2,7 @@ const courses = [
     { subject: 'CSE', number: 110, title: 'Intro to Programming', credits: 2, completed: true },
     { subject: 'WDD', number: 130, title: 'Web Fundamentals', credits: 2, completed: true },
     { subject: 'CSE', number: 111, title: 'Programming with Functions', credits: 2, completed: true },
-    { subject: 'CSE', number: 210, title: 'Programming with Classes', credits: 2, completed: false },
+    { subject: 'CSE', number: 210, title: 'Programming with Classes', credits: 2, completed: true },
     { subject: 'WDD', number: 131, title: 'Dynamic Web Fundamentals', credits: 2, completed: true },
     { subject: 'WDD', number: 231, title: 'Frontend Web Development I', credits: 2, completed: false }
 ];
